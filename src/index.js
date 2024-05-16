73e0e619ce64cef2581b347b4e6510dc998b5bab
Usama Ansar <140901613+UsamaAnsar79@users.noreply.github.com>
2024-05-16 17:51:47 +0500

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,14 @@
+// index.js
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import TodoApp from './TodoApp';
+import store from './store';
+import './App.css'
+
+ReactDOM.render(
+  <Provider store={store}>
+    <TodoApp />
+  </Provider>,
+  document.getElementById('root')
+);
