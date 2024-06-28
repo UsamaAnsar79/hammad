46cd306997bacc30f7189be5275ff3325c4c54ef
hammadsajjad001 <140393160+hammadsajjad001@users.noreply.github.com>
2024-06-28 12:42:23 +0500

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
-// index.js
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import TodoApp from './TodoApp';
-import store from './store';
-import './App.css'
+import ReactDOM from 'react-dom/client';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
+import App from './App';
 
-ReactDOM.render(
-  <Provider store={store}>
-    <TodoApp />
-  </Provider>,
-  document.getElementById('root')
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
+
